ac4bd3f20702814df4e2cff642e4e3bcf35a2b3e
ConradMare890317 <Conradmare@gmail.com>
2021-04-19 10:43:16 +0200

    2021/04/19 (am)

--- React_Classwork2/hello-react/src/App.js
+++ React_Classwork2/hello-react/src/App.js
@@ -2,28 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <p>          
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque."          
-        </p>
+class App extends Component {
 
-        <a
-          className="App-link"
-          href="https://github.com/ConradMare890317"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Conrad Maré
-        </a>
-        
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          <a
+            className="App-link"
+            href="https://github.com/ConradMare890317"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Conrad Maré
+          </a>
+          <p>          
+            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque."          
+          </p>
+          </header>
+      </div>
+    );
+  }
 }
 
 export default App;
