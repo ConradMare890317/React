0e32425b80862c37a4e089876a7e4658b21260bd
ConradMare890317 <Conradmare@gmail.com>
2021-04-19 12:07:44 +0200

    2021/04/19 (pm) My-Shop

---
+++ React_Classwork2/hello-react2.0/my-shop/src/Catalog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Catalog.css';
+import './Message.';
+
+
+class Catalog extends React.Component {
+    render() {
+        let title = "The Catalog of today " + new Date().toDateString();
+        return <div><h2>{title}</h2></div>;
+    }
+}
+
+export default Catalog;
