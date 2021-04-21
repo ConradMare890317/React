d4b1a042dbfb5b0ea705646631415afcf3eed1b3
ConradMare890317 <Conradmare@gmail.com>
2021-04-21 13:23:22 +0200

    2021/04/21 (pm) My-Shop_Project

--- React_Classwork2/hello-react2.0/my-shop/src/App.js
+++ React_Classwork2/hello-react2.0/my-shop/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import Catalog from './Catalog';
-
+import About from './About';
+import WineMakers from './WineMakers';
+import { Switch, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
@@ -9,10 +11,22 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">The Catalog App</h1>
+          <nav>
+            <ul>
+              <li><Link to='/'>Catalog</Link></li>
+              <li><Link to='/winemakers'>WineMakers</Link></li>
+              <li><Link to='/about'>About</Link></li>
+            </ul>
+          </nav>
         </header>
-        <Catalog />
+		<Switch>
+		   <Route exact path='/' component={Catalog}/>
+       <Route path='/winemakers' component={WineMakers}/>
+		   <Route path='/about' component={About}/>
+		</Switch>
       </div>
     );
   }
 }
+
 export default App;
