eb4283b637160515834d66e309d732e01391a7cb
ConradMare890317 <Conradmare@gmail.com>
2021-04-28 18:23:05 +0200

    2021/04/28 (pm)

--- React_Classwork2/hello-react/src/App.js
+++ React_Classwork2/hello-react/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { fadeIn } from 'animate-keyframes';
+//import { fadeIn } from 'animate-keyframes';
 
 class App extends Component {
 
